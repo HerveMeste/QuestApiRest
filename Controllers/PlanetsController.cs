44eecca3550a137b14c2b9f634fb2c85e2115b00
HerveMeste <herve.meste@gmail.com>
2020-12-14 18:09:22 +0100

    Quest Api Rest

---
+++ Controllers/PlanetsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using QuestApiRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace QuestApiRest.Controllers
+{
+    
+    
+    public class PlanetsController : Controller
+    {
+        [Route("Planets/{id}")]
+        public IActionResult Index(Int32 id)
+        {
+            WebClient client = new WebClient();
+
+            String response = client.DownloadString("https://swapi.dev/api/planets/" + id +"/");
+            Planets planets = JsonConvert.DeserializeObject<Planets>(response);
+
+            return View(planets);
+        }
+
+
+        
+    }
+}
